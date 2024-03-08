79195ff07c9536dd11f8b585be42e6a9c931b346
srr0bot <danielscmo07@gmail.com>
2024-03-08 15:29:11 -0500

    hello test

--- semanthicAnalyzer.py
+++ semanthicAnalyzer.py
@@ -17,15 +17,20 @@ class Analyzer:
                     {
                         "role": "user",
                                 "content": f"Analiza cual  lenguaje de programacion es (Julia o Ruby)  y encuentre errores que signifiquen una incorrecta compilación del codigo, si no, no los muestres y compila: {code}. En la respuesta no muestres el nombre del lenguaje. Los errores muestralos como los mostraria un compilador. si es correcto el codigo muestra la ejecucion de la siguiente forma: Ejecución:(Aqui va el resultado del codigo), si no, muestra: (aqui el error de compilacion)",
-                    },
+                    }
+                ]
+            )
+            completion1 = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
                     {
                         "role": "user",
-                                "content": f"Di hola juanpis",
-                    },
-                ],
+                                "content": f"Hola",
+                    }
+                ]
             )
             response = completion.choices[0].message.content
-            response1 = completion.choices[1].message.content
+            response1 = completion1.choices[0].message.content
             print(response1)
             return response
 
